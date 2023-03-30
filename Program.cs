a74b2a91494d27cea2ebf310a7d0e79668511bfb
chapcs <benchapman003@gmail.com>
2023-03-30 08:38:19 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        new ConstructorTest();
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+public class ConstructorTest
+{
+    public int i = 1;
+
+    public ConstructorTest()
+    {
+        Console.WriteLine($"i is {i}");
+    }
+}
+
+// use the debugger to walk through how a constructor works
+// it is a method called when a new instance of a class is created
+// always will have same name as class and no return
